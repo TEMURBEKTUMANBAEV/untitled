9a2c628e6150f95c396346e0264ecd56b5b82379
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-07 17:37:05 +0300

    Initial commit

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        String url = "https://next-dentist911.vercel.app/";
+        driver.get(url);
+
+        WebElement image = driver.findElement(By.cssSelector("img[src='/images/logo.png']"));
+        if (image.isDisplayed()) {
+            System.out.println("Изображение отображается корректно");
+        } else {
+            System.out.println("Изображение не отображается");
+        }
+
+        driver.quit();
+    }
+}
